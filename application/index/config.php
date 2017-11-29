03ee7ea90a8fe95adfa6b25df05bd31e2c41ce77
wangyu <swunwy@163.com>
2017-11-29 23:11:48 +0800

    update

---
+++ application/index/config.php
@@ -0,0 +1,16 @@
+<?php
+
+$root = request()->root();
+
+define('__ROOT__',str_replace('/index.php','',$root));
+
+return [
+
+    // 视图输出字符串内容替换
+    'view_replace_str'       => [
+    	'__PUBLIC__'	=> __ROOT__.'/static/index/',
+    	'__CSS__'		=> __ROOT__.'/static/index/css',
+    	'__JS__'		=> __ROOT__.'/static/index/js'
+    ],
+
+];
