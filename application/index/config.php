53f0b495c0d57f3f00969b1deed23e1c898f6569
wangyu <swunwy@163.com>
2017-11-30 21:11:23 +0800

    update

--- application/index/config.php
+++ application/index/config.php
@@ -10,7 +10,10 @@ return [
     'view_replace_str'       => [
     	'__PUBLIC__'	=> __ROOT__.'/index/',
     	'__CSS__'		=> __ROOT__.'/index/css',
-    	'__JS__'		=> __ROOT__.'/index/js'
+    	'__JS__'		=> __ROOT__.'/index/js',
+        '__IMG__'       => __ROOT__.'/index/img',
+        '__FONT__'      => __ROOT__.'/index/fonts',
+        '__PLUGINS__'   => __ROOT__.'/index/plugins'
     ],
 
 ];
--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -7,6 +7,25 @@ use think\Controller;
 class Index extends Controller {
 
     public function index() {
-        $this->fetch();
+        return view();
     }
+
+    public function main() {
+        //mysql版本信息
+        $mysql_version =  db()->query('SELECT VERSION() AS ver');
+        $config = [
+            'url' => $_SERVER['HTTP_HOST'],
+            'document' => $_SERVER['DOCUMENT_ROOT'],
+            'server_os' => PHP_OS,
+            'server_port' => $_SERVER['SERVER_PORT'],
+            'server_soft' => $_SERVER['SERVER_SOFTWARE'],
+            'php_version' => PHP_VERSION,
+            'mysql_version' => $mysql_version[0]['ver'],
+            'max_upload_size' => ini_get('upload_max_filesize')
+        ];
+
+        $this->assign('config', $config);
+        return view('main');
+    }
+
 }
